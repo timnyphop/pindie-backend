b865968819dd2fac2e3509b4bd69813f2dbe39ad
tinpo <137786541+timnyphop@users.noreply.github.com>
2024-05-21 15:57:15 +0700

    add frontend to cors

--- middllewares/cors.js
+++ middllewares/cors.js
@@ -1,4 +1,4 @@
-const allowedCors = ["http://localhost:3000", "http://localhost:3001"];
+const allowedCors = ["http://localhost:3000", "http://localhost:3001",'https://pindie.portal.nomoredomainswork.ru'];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
